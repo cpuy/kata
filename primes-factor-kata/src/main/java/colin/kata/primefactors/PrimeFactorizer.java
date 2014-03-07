f0690420d524ac988f50eaa6743d47312f770e6f
Colin PUY <colin.puy@bonitasoft.com>
2014-03-07 14:01:35 +0100

    remove recursive call of factorize

--- primes-factor-kata/src/main/java/colin/kata/primefactors/PrimeFactorizer.java
+++ primes-factor-kata/src/main/java/colin/kata/primefactors/PrimeFactorizer.java
@@ -7,12 +7,13 @@ public class PrimeFactorizer {
 
 	public List<Integer> factorize(final int number) {
 		List<Integer> primes = new ArrayList<Integer>();
-		if (number > 1) {
-			if (number % 2 == 0) {
+		int n = number;
+		if (n > 1) {
+			if (n % 2 == 0) {
 				primes.add(2);
-				primes.addAll(factorize(number / 2));
+				n /= 2;
 			} else {
-				primes.add(number);
+				primes.add(n);
 			}
 		}
 		return primes;
