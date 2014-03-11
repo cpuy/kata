64dec843f1336150433a836276652ef78c5b3957
Colin PUY <colin.puy@bonitasoft.com>
2014-03-11 13:12:33 +0100

    refactor, change while in for

--- primes-factor-kata/src/main/java/colin/kata/primefactors/PrimeFactorizer.java
+++ primes-factor-kata/src/main/java/colin/kata/primefactors/PrimeFactorizer.java
@@ -9,22 +9,20 @@ public class PrimeFactorizer {
 
 	public List<Integer> factorize(final int number) {
 		Factorizable factorizable = new Factorizable(number);
-		if (factorizable.canBeFactorized()) 
+		if (factorizable.canBeFactorized())
 			return factorize(factorizable);
 		else
 			return emptyList();
 	}
 
 	private List<Integer> factorize(Factorizable factorizable) {
-		List<Integer> primes = new ArrayList<Integer>();
-		int prime = 2;
-		while (factorizable.greaterOrEqualsThan(prime)) {
-			if (factorizable.isFactorizableBy(prime)) {
-				primes.add(prime);
-				factorizable.factorize(prime);
-			} 
-			prime++;
+		List<Integer> factors = new ArrayList<Integer>();
+		for (int factor = 2; factorizable.greaterOrEqualsThan(factor); factor++) {
+			if (factorizable.isFactorizableBy(factor)) {
+				factors.add(factor);
+				factorizable.factorize(factor);
+			}
 		}
-		return primes;
+		return factors;
 	}
 }
--- primes-factor-kata/src/test/java/colin/kata/primefactors/PrimeFactorizerTest.java
+++ primes-factor-kata/src/test/java/colin/kata/primefactors/PrimeFactorizerTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class PrimeFactorizerTest {
 
 	private PrimeFactorizer primeFactorizer;
@@ -16,53 +15,53 @@ public class PrimeFactorizerTest {
 	public void initFactorizer() {
 		primeFactorizer = new PrimeFactorizer();
 	}
-	
+
 	@Test
 	public void numbers_smaller_than_2_does_not_have_any_primefactors() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(1);
-		
+
 		assertThat(primeFactors).isEmpty();
 	}
-	
+
 	@Test
 	public void prime_factor_of_2_is_2() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(2);
-		
+
 		assertThat(primeFactors).containsOnly(2);
 	}
-	
+
 	@Test
 	public void prime_factor_of_3_is_3() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(3);
-		
+
 		assertThat(primeFactors).containsOnly(3);
 	}
-	
+
 	@Test
 	public void prime_factors_of_4_are_2_and_2() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(4);
-		
+
 		assertThat(primeFactors).containsOnly(2, 2);
 	}
-	
+
 	@Test
 	public void prime_factors_of_6_are_3_and_2() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(6);
-		
+
 		assertThat(primeFactors).containsOnly(3, 2);
 	}
-	
+
 	@Test
 	public void prime_factors_of_9_are_3_and_3() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(9);
-		
+
 		assertThat(primeFactors).containsOnly(3, 3);
 	}
-	
+
 	@Test
 	public void prime_factors_of_330_are_11_2_3_and_5() throws Exception {
 		List<Integer> primeFactors = primeFactorizer.factorize(330);
-		
+
 		assertThat(primeFactors).containsOnly(11, 2, 3, 5);
 	}
 }
