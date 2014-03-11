f1ad26ed34ab51003f35d5c0116dd3a81dfdfa2b
Colin PUY <colin.puy@bonitasoft.com>
2014-03-11 13:06:40 +0100

    refactor, encapsulate number

---
+++ primes-factor-kata/src/main/java/colin/kata/primefactors/Factorizable.java
@@ -0,0 +1,26 @@
+package colin.kata.primefactors;
+
+public class Factorizable {
+
+	private int number;
+
+	public Factorizable(int number) {
+		this.number = number;
+	}
+	
+	public boolean canBeFactorized() {
+		return number > 1;
+	}
+	
+	public boolean isFactorizableBy(int factor) {
+		return number % factor == 0;
+	}
+	
+	public void factorize(int factor) {
+		number /= factor;
+	}
+	
+	public boolean greaterOrEqualsThan(int number) {
+		return this.number >= number;
+	}
+}
--- primes-factor-kata/src/main/java/colin/kata/primefactors/PrimeFactorizer.java
+++ primes-factor-kata/src/main/java/colin/kata/primefactors/PrimeFactorizer.java
@@ -1,33 +1,30 @@
 package colin.kata.primefactors;
 
+import static java.util.Collections.emptyList;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactorizer {
 
 	public List<Integer> factorize(final int number) {
+		Factorizable factorizable = new Factorizable(number);
+		if (factorizable.canBeFactorized()) 
+			return factorize(factorizable);
+		else
+			return emptyList();
+	}
+
+	private List<Integer> factorize(Factorizable factorizable) {
 		List<Integer> primes = new ArrayList<Integer>();
-		int n = number;
-		if (canBeFactorized(n)) {
-			int prime = 2;
-			
-			while (prime <= n) {
-				if (isFactorizable(n, prime)) {
-					primes.add(prime);
-					n /= prime;
-				} 
-				prime++;
-			}
+		int prime = 2;
+		while (factorizable.greaterOrEqualsThan(prime)) {
+			if (factorizable.isFactorizableBy(prime)) {
+				primes.add(prime);
+				factorizable.factorize(prime);
+			} 
+			prime++;
 		}
 		return primes;
 	}
-
-	private boolean canBeFactorized(int number) {
-		return number > 1;
-	}
-
-	private boolean isFactorizable(int number, int factor) {
-		return number % factor == 0;
-	}
-
 }
