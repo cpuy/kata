da230fa62355c6b1d74616e064010ef51d4fd106
Colin PUY <colin.puy@bonitasoft.com>
2014-04-15 14:11:57 +0200

    [codegen] refactor

--- code-generator-kata/src/main/java/colin/kata/codegen/MessageParser.java
+++ code-generator-kata/src/main/java/colin/kata/codegen/MessageParser.java
@@ -1,32 +1,32 @@
 package colin.kata.codegen;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MessageParser {
 
-
-	private static final String TAG_DATANAME = "D";
+	private static final String COMMENT = "#";
+	private static final String DATANAME = "D";
 
 	public Message parse(List<String> lines) throws InvalidFormatExeption {
 		Message message = new Message();
-		for (String string : lines) {
-			if (!describeDataName(string)) {
-				if (string.contains("#")) {
-					message.addComment(string.replaceAll("#", "").trim());
-				} else {
-					throw new InvalidFormatExeption("Message must start with a D tag");
-				}
+		for (String line : lines) {
+			if (describe(DATANAME, line)) {
+				message.setDataName(extract(line, DATANAME));
+			} else if (describe(COMMENT, line)) {
+				message.addComment(extract(line, COMMENT));
 			} else {
-				message.setDataName(string.replaceAll("D", "").trim());
+				throw new InvalidFormatExeption("Message must start with a D tag, or have optionnal comments");
 			}
 		}
 		return message;
 	}
 
-	private boolean describeDataName(String line) {
-		return line.contains(TAG_DATANAME);
+	private boolean describe(String tag, String line) {
+		return line.contains(tag);
 	}
 
+	private String extract(String line, String tag) {
+		return line.replace(tag, "").trim();
+	}
 
 }
