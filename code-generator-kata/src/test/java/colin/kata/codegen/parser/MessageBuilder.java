8b65168da6b5dfb7aecc49e9dd8ded5e7e658486
Colin PUY <colin.puy@bonitasoft.com>
2014-04-16 13:37:47 +0200

    [codegen] refactor tests

--- code-generator-kata/src/test/java/colin/kata/codegen/parser/MessageBuilder.java
+++ code-generator-kata/src/test/java/colin/kata/codegen/parser/MessageBuilder.java
@@ -1,6 +1,26 @@
 package colin.kata.codegen.parser;
 
+import colin.kata.codegen.Message;
+
 public class MessageBuilder {
 
+	private Message message = new Message();
 	
+	public static MessageBuilder aMessage() {
+		return new MessageBuilder();
+	}
+
+	public MessageBuilder withDataName(String dataName) {
+		message.setDataName(dataName);
+		return this;
+	}
+	
+	public MessageBuilder withComment(String comment) {
+		message.addComment(comment);
+		return this;
+	}
+	
+	public Message build() {
+		return message;
+	}
 }
--- code-generator-kata/src/test/java/colin/kata/codegen/strategy/DartStrategyTest.java
+++ code-generator-kata/src/test/java/colin/kata/codegen/strategy/DartStrategyTest.java
@@ -1,22 +1,31 @@
 package colin.kata.codegen.strategy;
 
+import static colin.kata.codegen.parser.MessageBuilder.aMessage;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import colin.kata.codegen.Message;
+import colin.kata.codegen.parser.MessageBuilder;
 
 
 public class DartStrategyTest {
 
+	private DartStrategy strategy;
+
+	@Before
+	public void setUp() {
+		strategy = new DartStrategy();
+	}
+	
 	@Test
 	public void should_generate_class_structure() throws Exception {
 		String expected = "class Product {\n}";
-		Message message = new Message();
-		message.setDataName("Product");
+		Message message = aMessage().withDataName("Product").build();
 		
-		String generated = new DartStrategy().generate(message);
+		String generated = strategy.generate(message);
 		
 		assertThat(generated).isEqualTo(expected);
 	}
@@ -24,14 +33,18 @@ public class DartStrategyTest {
 	@Test
 	public void should_add_comments_on_top() throws Exception {
 		String expected = "/**\n * This is a comment\n * This is another comment\n */\nclass Product {\n}";
+		Message message = aMessage().withDataName("Product")
+				.withComment("This is a comment")
+				.withComment("This is another comment").build();
 		
-		Message message = new Message();
-		message.setDataName("Product");
-		message.addComment("This is a comment");
-		message.addComment("This is another comment");
-		
-		String generated = new DartStrategy().generate(message);
+		String generated = strategy.generate(message);
 		
 		assertThat(generated).isEqualTo(expected);
 	}
+	
+	@Test
+	public void should_add_fields_in_class_body() throws Exception {
+		
+	}
+	
 }
